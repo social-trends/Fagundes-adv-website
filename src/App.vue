ae5df3521ace07161a4f8171fa083a301ac4dc40
Leonardo Almeida <leonardo@bulldesk.com.br>
2019-03-14 12:28:04 -0300

    First Files
    files

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <MenuBar/>
+  </div>
+</template>
+
+<script>
+import MenuBar from './components/MenuBar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    MenuBar
+  }
+}
+</script>
---
+++ src/components/MenuBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+      <input type="text" name="" v-model="searchValue">
+      <button v-on:click="search(searchValue)">pesquisar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuBar',
+  props: {
+  },
+
+  data () {
+    return {
+      searchValue: 0,
+    }
+  },
+
+  mounted () {
+    this.$http.get('https://fagundesadv.com.br/api/get_recent_posts/?count=3')
+      .then((data) => {
+        this.postsdata = data
+      })
+  },
+
+  methods: {
+    search(searchValue){
+      var search = searchValue;
+      window.location.href = "https://fagundesadv.com.br/?s=" + search;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
